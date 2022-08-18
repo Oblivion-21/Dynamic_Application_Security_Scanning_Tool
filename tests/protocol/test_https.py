53ee90c15295acf608f8e0f92db998e9076dad4a
Oblivion-21 <djpa122@protonmail.com>
2022-08-18 14:52:55 +1000

    adding protocol test suit

---
+++ tests/protocol/test_https.py
@@ -0,0 +1,18 @@
+import TestManager
+import requests
+
+
+async def https_test(ws, url):
+    try:
+        r = requests.get(f"https://{url}")
+        if 'https' in r.url:
+            message = 'PASSED'
+        else:
+            message = 'FAILED'
+    except:
+        message = 'INCOMPLETE'
+    finally:
+        await TestManager.send_msg(ws, {"message": message}, True, 'https-test' )
+
+def test_function(num):
+    return num + 1
