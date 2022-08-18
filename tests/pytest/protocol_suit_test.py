fccf9e10a53f7ca479a31cdca10cd270dd4a31cf
Mitchell <mitchell444999@gmail.com>
2022-08-18 17:57:51 +1000

    Added mocking for unit tests with basic assert statement

--- tests/pytest/protocol_suit_test.py
+++ tests/pytest/protocol_suit_test.py
@@ -1,20 +1,18 @@
 import mock
 import pytest
-import asyncio
-import websockets
-from pytest_mock import mocker
-
-import TestManager
 from tests.protocol import test_https
-pytest_plugins = ('pytest_asyncio')
+pytest_plugins = 'pytest_asyncio'
 
-HOST = 'localhost'
-PORT = '8989'
 
 @pytest.mark.asyncio
-async def test_https_true():
+async def test_https_true(mocker):
     '''check the https status against a known true'''
+    # SETUP
     async_mock = mock.AsyncMock()
     mocker.patch('TestManager.send_msg', side_effect=async_mock)
-    await test_https.https_test(None, 'www.google.com')
 
+    await test_https.https_test(None, 'www.google.com')
+    msg = async_mock.call_args[0][1]['message']
+    assert msg == 'PASSED'
+    # TEARDOWN
+    mocker.stopall()
