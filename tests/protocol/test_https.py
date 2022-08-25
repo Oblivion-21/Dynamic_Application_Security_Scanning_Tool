5ca34e94485dea51136bc218ee8c38a0b28dd571
Oblivion-21 <djpa122@protonmail.com>
2022-08-26 00:01:44 +1000

    added ssl version tests (incl test we cant run without a container) and unit tests for each

--- tests/protocol/test_https.py
+++ tests/protocol/test_https.py
@@ -2,14 +2,15 @@ import TestManager
 import requests
 
 
-async def https_test(ws, url):
+async def test_https(ws, url):
+    '''Check if HTTPS is enabled on site'''
     try:
         r = requests.get(f"https://{url}")
         if 'https' in r.url:
             message = 'PASSED'
         else:
             message = 'FAILED'
-    except:
-        message = 'INCOMPLETE'
+    except Exception as e:
+        message = f'INCOMPLETE - {e}'
     finally:
-        await TestManager.send_msg(ws, {"message": message}, True, 'https-test')
+        await TestManager.send_msg(ws, {"message": message}, True, 'test-https')
