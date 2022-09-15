2d3b7e70babe87f5f63862546da3aa26d17eec97
Mitchell <mitchell444999@gmail.com>
2022-09-15 15:41:24 +1000

    Moved event handler and set socket as state

--- gui/src/components/App.js
+++ gui/src/components/App.js
@@ -1,15 +1,10 @@
+import {useState} from "react";
 import Container from "react-bootstrap/Container";
 import SuiteForm from "./SuiteForm";
 import Main from "./Main";
 
 function App() {
-  const socket = new WebSocket("ws://localhost:8989");
-  socket.addEventListener('message', (event) => {
-    const data = event.data;
-    console.log(`Recived message ${data}`);
-    document.getElementById("testing-out").innerHTML += `<div class="data">${data}</div>`;
-  });
-
+  const [socket] = useState(new WebSocket("ws://localhost:8989"));
   return (
     <Container fluid className="h-100 mh-100 m-0 p-0 d-flex">
       <Main socket={socket}/>
--- gui/src/components/Main.js
+++ gui/src/components/Main.js
@@ -2,13 +2,14 @@ import Container from "react-bootstrap/Container";
 import Tabs from "react-bootstrap/Tabs";
 import Tab from "react-bootstrap/Tab";
 import Testing from "./Testing";
+import Suites from "./Suites";
 
 function Main({socket}) {
   return (
     <Container fluid className="w-75 py-2 px-0">
       <Tabs defaultActiveKey="testing" className="">
         <Tab eventKey="suites" title="Suites">
-
+          <Suites socket={socket}/>
         </Tab>
         <Tab eventKey="history" title="History">
 
