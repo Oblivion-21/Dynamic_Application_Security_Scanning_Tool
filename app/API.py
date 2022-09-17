2d8b4b6043bc582972417676aa5e0161bf7e3501
Mitchell <mitchell444999@gmail.com>
2022-09-17 19:23:34 +1000

    Added useDatabase boolean and passed it around system, also added history/history req message

--- app/API.py
+++ app/API.py
@@ -1,19 +1,25 @@
 import asyncio
+import json
 import threading
 
 from websockets import serve
 
+import storageManager
 import testManager
 
 host = "127.0.0.1"
 port = 8989
 processLock = threading.Lock()
+useDatabase = False
 
 
 async def messageHand(ws):
     async for msg in ws:
-        with processLock:
-            await testManager.runTests(ws, msg)
+        if json.loads(msg)["messageType"] == "REQ-HISTORY":
+            await history(ws)
+        else:
+            with processLock:
+                await testManager.runTests(ws, msg, useDatabase)
 
 
 async def sendMessage(ws, msg):
@@ -25,6 +31,17 @@ async def server():
         await asyncio.Future()
 
 
+async def history(ws):
+    history = {
+        "messageType": "HISTORY",
+        "results": storageManager.show()
+    }
+    await sendMessage(ws, json.dumps(history))
+
+
 if __name__ == "__main__":
+    useDatabase = storageManager.setup()
+    if useDatabase:
+        testManager.setSuiteID()
     print(f"Websocket API running: {host}:{port}")
     asyncio.run(server())
