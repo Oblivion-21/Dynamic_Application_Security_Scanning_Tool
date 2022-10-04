52dbc937187e91d70a0972f9be9ede3867c56570
chimpfacts <104479394+chimpfacts@users.noreply.github.com>
2022-10-04 21:18:44 +1100

    finished the test

--- app/tests/authentication/authTest.py
+++ app/tests/authentication/authTest.py
@@ -9,7 +9,6 @@ passlist = ["^cH%seY9VP*%f7$k", "ueg+tja(Ush46fJ%",
 "zh@RQ8Nj69##(@+e", "9YpRkh@vEb6tTRJc", 
 "bABAGW+CgXc!h9mt", "&f@Ty7Su(SHmsRqj" ]
 
-
 async def bruteForceTest(ws, session, testConfigs, url):
     if "://" not in url:
         url = f"https://{url}"
@@ -38,23 +37,23 @@ async def bruteForceTest(ws, session, testConfigs, url):
 
     #sends username/password combinations to request_URL to detect status code. If the status code alters then the user has been kicked out
     #and the test is a success
-    for posturl in posturllist:
-        statuscodeinit = 0
-        for password in passlist:
-            data = {'username': username, 'password': password, "Login": 'submit'}
-            send_data = requests.post(posturl, data=data)
-            statuscode = send_data.status_code
-            print(f"Brute Force Test, status code {url}: {statuscode}")
-            if statuscodeinit == 0:
-                statuscodeinit = statuscode
-            if statuscode != statuscodeinit:
-                await testManager.sendMessage(ws, {"Message": "success"}, url, True, "bruteForceTest")
-                return
-    
-    await testManager.sendMessage(ws, {"Message": "fail"}, url, True, "bruteForceTest")
-                
+    try:
+        for posturl in posturllist:
+            statuscodeinit = 0
+            for password in passlist:
+                data = {'username': username, 'password': password, "Login": 'submit'}
+                send_data = requests.post(posturl, data=data)
+                statuscode = send_data.status_code
+                print(f"Brute Force Test, status code {url}: {statuscode}")
+                if statuscodeinit == 0:
+                    statuscodeinit = statuscode
+                if statuscodeinit == 405:
+                    await testManager.sendMessage(ws, {"Message": "invalid"}, url, True, "bruteForceTest")
+                    return
+                if statuscode != statuscodeinit:
+                    await testManager.sendMessage(ws, {"Message": "success"}, url, True, "bruteForceTest")
+                    return
         
-
-
-
-
+        await testManager.sendMessage(ws, {"Message": "fail"}, url, True, "bruteForceTest")
+    except Exception as e:
+        await testManager.sendMessage(ws, {"Message": "Can not Complete Test"}, url, True, "bruteForceTest")
