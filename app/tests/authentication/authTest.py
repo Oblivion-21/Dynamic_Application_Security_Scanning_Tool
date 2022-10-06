ba972b477da355d9039f54f6523adabd3e586f43
Mitchell <mitchell444999@gmail.com>
2022-10-06 20:59:24 +1100

    Removed test file, converted to camelCase, and standardised messages

--- app/tests/authentication/authTest.py
+++ app/tests/authentication/authTest.py
@@ -2,58 +2,59 @@ import requests
 from bs4 import BeautifulSoup
 import testManager
 
-#List of passcodes used for brute force
-passlist = ["^cH%seY9VP*%f7$k", "ueg+tja(Ush46fJ%", 
-"sMcr3tseGnRR3TEf", "K3ffTykBM^yM#yuS", 
-"86fKb8zB9HBA*RxX", "HEKFkUbyw4S3h2ug", 
-"zh@RQ8Nj69##(@+e", "9YpRkh@vEb6tTRJc", 
-"bABAGW+CgXc!h9mt", "&f@Ty7Su(SHmsRqj" ]
+# List of passcodes used for brute force
+passList = ["^cH%seY9VP*%f7$k", "ueg+tja(Ush46fJ%",
+    "sMcr3tseGnRR3TEf", "K3ffTykBM^yM#yuS",
+    "86fKb8zB9HBA*RxX", "HEKFkUbyw4S3h2ug",
+    "zh@RQ8Nj69##(@+e", "9YpRkh@vEb6tTRJc",
+    "bABAGW+CgXc!h9mt", "&f@Ty7Su(SHmsRqj"
+]
 
 async def bruteForceTest(ws, session, testConfigs, url):
     if "://" not in url:
         url = f"https://{url}"
 
-    #Define Variables
+    # Define Variables
     html = requests.get(url)
 
     soup = BeautifulSoup(html.content, 'html.parser')
 
-    postformlist = soup.find_all('form')
+    postFormList = soup.find_all('form')
 
-    posturllist = [] 
+    postUrlList = []
 
     username = testConfigs['username']
 
-    #extracts html data from site to obtain the request_URL
-    for postform in postformlist:
-        action = postform['action']
+    # extracts html data from site to obtain the request_URL
+    for postForm in postFormList:
+        action = postForm['action']
         if action == "":
             continue
         elif action[0] == '.':
             action = action.replace('.', url)
         elif action[0] == '/':
             action = url + action
-        posturllist.append(action)
+        postUrlList.append(action)
 
-    #sends username/password combinations to request_URL to detect status code. If the status code alters then the user has been kicked out
-    #and the test is a success
+    # sends username/password combinations to request_URL to detect status code. If the status code alters then the user has been kicked out
+    # and the test is a success
     try:
-        for posturl in posturllist:
-            statuscodeinit = 0
-            for password in passlist:
+        for postUrl in postUrlList:
+            statusCodeInit = 0
+            for password in passList:
                 data = {'username': username, 'password': password, "Login": 'submit'}
-                send_data = requests.post(posturl, data=data)
-                statuscode = send_data.status_code
-                print(f"Brute Force Test, status code {url}: {statuscode}")
-                if statuscodeinit == 0:
-                    statuscodeinit = statuscode
-                if statuscodeinit == 405:
-                    await testManager.sendMessage(ws, {"Message": "invalid"}, url, True, "bruteForceTest")
+                sendData = requests.post(postUrl, data=data)
+                statusCode = sendData.status_code
+                print(f"Brute Force Test, status code {url}: {statusCode}")
+                if statusCodeInit == 0:
+                    statusCodeInit = statusCode
+                if statusCodeInit == 405:
+                    await testManager.sendMessage(ws, {"Message": "INVALID"}, url, True, "bruteForceTest")
                     return
-                if statuscode != statuscodeinit:
-                    await testManager.sendMessage(ws, {"Message": "success"}, url, True, "bruteForceTest")
+                if statusCode != statusCodeInit:
+                    await testManager.sendMessage(ws, {"Message": "PASSED"}, url, True, "bruteForceTest")
                     return
         
-        await testManager.sendMessage(ws, {"Message": "fail"}, url, True, "bruteForceTest")
+        await testManager.sendMessage(ws, {"Message": "FAILED"}, url, True, "bruteForceTest")
     except Exception as e:
-        await testManager.sendMessage(ws, {"Message": "Can not Complete Test"}, url, True, "bruteForceTest")
+        await testManager.sendMessage(ws, {"Message": f"INCOMPLETE - {e}"}, url, True, "bruteForceTest")
