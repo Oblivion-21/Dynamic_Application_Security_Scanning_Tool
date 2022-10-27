592835b423ea2ade8534937f545ad0a8c8393a34
anthonysidoti <asidoti1@optusnet.com.au>
2022-10-27 19:34:21 +1100

    I fixed the windows networking :)

--- gui/src/components/App.js
+++ gui/src/components/App.js
@@ -4,7 +4,7 @@ import SuiteForm from "./SuiteForm";
 import Main from "./Main";
 
 function App() {
-  const [socket] = useState(new WebSocket(`ws://${[process.env.REACT_APP_BACK_END]}:8989`));
+  const [socket] = useState(new WebSocket(`ws://localhost:8989`));
   return (
     <Container fluid className="vh-100 min-vh-100 vw-100 min-vw-100 m-0 p-0 d-flex">
       <Main socket={socket}/>
